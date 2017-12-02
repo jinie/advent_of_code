88bcb3b1597ce75b254a6ee3b913f3db14391e6d
Jimmy Selgen Nielsen <jimmy.selgen@gmail.com>
2017-12-02 09:55:54 +0100

    add solution for day 2

---
+++ 2017/02_corruption_checksum.py
@@ -0,0 +1,13 @@
+with open('input02.txt','rt') as f:
+	rows=f.readlines()
+	rows=[row.strip('\n').split('\t') for row in rows]
+	for i,r in enumerate(rows):
+		rows[i]=[int(d) for d in r]
+
+sums = sum([max(r)-min(r) for r in rows])
+print(sums)
+
+sums=0
+for i,row in enumerate(rows):
+		sums+= sum([x/y for x in row for y in row if y >0 and y!=x and x%y==0])
+print(sums)
