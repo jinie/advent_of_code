85e0b4ca7e3819fe6fc6bfb642df8674ff1e57a8
Jimmy Selgen Nielsen <jimmy.selgen@gmail.com>
2017-12-03 17:22:42 +0100

    implement part 2

--- 2017/day3_spiral_memory.py
+++ 2017/day3_spiral_memory.py
@@ -1,4 +1,5 @@
 import math
+from collections import defaultdict
 target = 361527
 
 #part 1
@@ -8,8 +9,39 @@ if i % 2 == 0:
   
 corners = [i**2 - ((i-1) * j) - math.floor(i/2) for j in range(4)]
 steps = ((i-1)/2) + min([abs(corner - target) for corner in corners])
-print(steps)
+print('part 1:',steps)
 
 
 #part 2
 #https://oeis.org/A141481
+
+def sum_table(table,x,y):
+  return sum(table[i,j] for i in range(x-1, x+2) for j in range(y-1, y+2))
+  
+def spiral_table(target):
+  table = defaultdict(int)
+  side=1
+  x,y = 0,0
+  table[0,0] = 1
+  csum = 1
+  
+  while csum < target:
+    for _ in range(side): #increment x
+      x+=1
+      table[x,y] = sum_table(table,x,y)
+      if table[x,y] > target: return table[x,y]
+    for _ in range(side): #increment y
+      y+=1
+      table[x,y] = sum_table(table,x,y)
+      if table[x,y] > target: return table[x,y]
+    for _ in range(side+1): #decrement x
+      x-=1
+      table[x,y] = sum_table(table,x,y)
+      if table[x,y] > target: return table[x,y]
+    for _ in range(side+1): #decrement y
+      y-=1
+      table[x,y] = sum_table(table,x,y)
+      if table[x,y] > target: return table[x,y]
+    side+=2
+    
+print('part 2:',spiral_table(target))
