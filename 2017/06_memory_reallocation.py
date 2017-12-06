e5c6b8a2ba1f682730059fb601df7f8115f4e8fa
Jimmy Selgen Nielsen <jimmy.selgen@gmail.com>
2017-12-06 12:05:17 +0100

    Remove "dead" code

--- 2017/06_memory_reallocation.py
+++ 2017/06_memory_reallocation.py
@@ -4,24 +4,21 @@ import operator
 
 def part1(input):
     results = [input]
-    ca = input[:]
+    c2 = input
     while True:
-        c2 = ca[:]
-        i, v = max(enumerate(ca), key=operator.itemgetter(1))
-        i2 = i
+        c2 = c2[:]
+        i, v = max(enumerate(c2), key=operator.itemgetter(1))
         c2[i] = 0
         while v > 0:
-            i2 = i2 + 1 if i2 + 1 < len(c2) else 0
-            c2[i2] += 1
+            i = i + 1 if i + 1 < len(c2) else 0
+            c2[i] += 1
             v -= 1
         if c2 in results:
             return len(results), c2
         else:
             results.append(c2)
-        ca = c2
 
-input = '11	11	13	7	0	15	5	5	4	4	1	1	7	1	15	11'.split()
-input = [int(i) for i in input]
+input = [int(i) for i in '11	11	13	7	0	15	5	5	4	4	1	1	7	1	15	11'.split()]
 
 # Part 1
 i, input = part1(input)
