56aa22e00059e9abf092707e58b5164564c96939
Jimmy Selgen Nielsen <jimmy.selgen@gmail.com>
2017-12-14 18:55:40 +0100

    Pep8 format

--- 2017/13_packet_scanners.py
+++ 2017/13_packet_scanners.py
@@ -6,7 +6,6 @@ def get_input():
     with open('input13.txt', 'rt') as f:
         lines = f.readlines()
         lines = [l.split(': ') for l in lines]
-    # return lines
     ret = {int(l[0]): int(l[1]) for l in lines}
     return ret
 
@@ -21,10 +20,11 @@ def part1():
     print(sum(i * input[i] for i in input.keys()
               if scanner_pos(input[i], i) == 0))
 
+
 def part2():
     input = get_input()
     for delay in count(1):
-        if any(scanner_pos(input[d] , d + delay) == 0 for d in input):
+        if any(scanner_pos(input[d], d + delay) == 0 for d in input):
             continue
         else:
             print(delay)
