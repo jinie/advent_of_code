9395d17519cc9f33d569d219df55bf7840a51161
Jimmy Selgen Nielsen <jimmy.selgen@gmail.com>
2017-12-14 11:37:53 +0100

    Add solution for Day 11

---
+++ 2017/11_hex_ed.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+from math import floor, sqrt
+from collections import namedtuple
+
+Point = namedtuple('Point',['x','y'])
+def get_input():
+    with open('input11.txt','rt') as f:
+        input = f.read().strip().split(',')
+    return input
+sign = lambda x: (1, -1)[x < 0]
+
+def hexdistance(x,y):
+    x,y = abs(x), abs(y)
+
+    if y > x:
+        return x + (y-x)/2
+    elif y < x:
+        return y + (x-y)/2
+    else:
+        return x
+
+def part1(input):
+    x,y = 0,0
+    furthestdistance = 0
+
+    for dir in input:
+        if dir == 'n':
+            y += 2
+        elif dir == 's':
+            y -=2
+        elif dir == 'se':
+            x+=1; y-=1
+        elif dir == 'sw':
+            x-=1; y-=1
+        elif dir == 'nw':
+            x-=1; y+=1
+        elif dir == 'ne':
+            x+=1; y+=1
+        dist = hexdistance(x,y)
+        if dist > furthestdistance:
+            furthestdistance = dist
+    return hexdistance(x,y), furthestdistance
+
+input = get_input()
+dist, furthest = part1(input)
+print("Part 1:",dist)
+print("Furthest Distance:",furthest)
+                
+        
+
