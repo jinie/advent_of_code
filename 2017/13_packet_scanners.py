797de10c7a40bafe7912537f3eedeee649c3b84d
Jimmy Selgen Nielsen <jimmy.selgen@gmail.com>
2017-12-14 18:55:12 +0100

    Add solution for Day 13, Part 2

--- 2017/13_packet_scanners.py
+++ 2017/13_packet_scanners.py
@@ -1,16 +1,35 @@
 #!/usr/#bin/env python3
+from itertools import count
+
+
 def get_input():
     with open('input13.txt', 'rt') as f:
         lines = f.readlines()
         lines = [l.split(': ') for l in lines]
     # return lines
-    ret = {int(l[0]): {'l': int(l[1])} for l in lines}
+    ret = {int(l[0]): int(l[1]) for l in lines}
     return ret
 
 
 def scanner_pos(depth, step):
-    return step if step <= depth else (step - (step * depth)) % depth
+    offset = step % ((depth - 1) * 2)
+    return 2 * (depth - 1) - offset if offset > depth - 1 else offset
 
 
-input = get_input()
-print(sum(i * input[i]['l'] for i in input.keys() if scanner_pos(input[i]['l'] - 1, i) == 0))
+def part1():
+    input = get_input()
+    print(sum(i * input[i] for i in input.keys()
+              if scanner_pos(input[i], i) == 0))
+
+def part2():
+    input = get_input()
+    for delay in count(1):
+        if any(scanner_pos(input[d] , d + delay) == 0 for d in input):
+            continue
+        else:
+            print(delay)
+            return
+
+
+part1()
+part2()
