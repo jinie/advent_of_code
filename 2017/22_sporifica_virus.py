1e7f727f531ed3fd26d300712d3c3bd8830781ec
Jimmy Selgen Nielsen <jimmy.selgen@gmail.com>
2018-01-05 12:02:40 +0100

    Further optimizations, 60% saved.. not because it was needed, but because it was funny

--- 2017/22_sporifica_virus.py
+++ 2017/22_sporifica_virus.py
@@ -7,18 +7,6 @@ def get_input():
 
 
 class Sporifica:
-    # States
-    # (C)lean -> (I)nfected
-    simple_states = {'C': 'I', 'I': 'C'}
-    # (C)lean -> (W)eakened -> (I)nfected -> (F)lagged
-    evolved_states = {'C': 'W', 'W': 'I', 'I': 'F', 'F': 'C'}
-    states = simple_states
-    dir_states = {'I': 'R', 'F': 'T', 'C': 'L', 'W': None}
-    ttable = {
-        'R': {'N': 'E', 'E': 'S', 'S': 'W', 'W': 'N'},
-        'L': {'N': 'W', 'W': 'S', 'S': 'E', 'E': 'N'},
-        'T': {'N': 'S', 'S': 'N', 'W': 'E', 'E': 'W'}
-    }
 
     def defaultvalue(self):
         return 'C'
@@ -31,42 +19,41 @@ class Sporifica:
                     infected[(x, y)] = 'I'
         return infected
 
-    def turn(self, heading, direction):
-        return self.ttable[direction][heading]
-
-    def move(self, x, y, heading):
-        if heading == 'S':
-            return (x, y + 1)
-        elif heading == 'N':
-            return (x, y - 1)
-        elif heading == 'E':
-            return (x + 1, y)
-        else:
-            return (x - 1, y)
-
-    def burst(self, x, y, heading, infected):
-        v = infected[(x,y)]
-        direction = self.dir_states[v]
-        nv = self.states[v]
-        
-        infected[(x, y)] = nv
-
-        if direction is not None:
-            heading = self.turn(heading, direction)
-        x, y = self.move(x, y, heading)
-
-        return x, y, heading, nv == 'I'
-
     def simulate(self, bursts, inp, simple=True):
+        # States
+        # (C)lean -> (I)nfected
+        simple_states = {'C': 'I', 'I': 'C'}
+        # (C)lean -> (W)eakened -> (I)nfected -> (F)lagged
+        evolved_states = {'C': 'W', 'W': 'I', 'I': 'F', 'F': 'C'}
+        dir_states = {'I': 'R', 'F': 'T', 'C': 'L', 'W': None}
+        ttable = {
+            'R': {'N': 'E', 'E': 'S', 'S': 'W', 'W': 'N'},
+            'L': {'N': 'W', 'W': 'S', 'S': 'E', 'E': 'N'},
+            'T': {'N': 'S', 'S': 'N', 'W': 'E', 'E': 'W'}
+        }        
         infected = self.load_initial(inp)
         x, y = len(inp) // 2, (len(inp) // 2)
         heading = 'N'
-        self.states = self.simple_states if simple else self.evolved_states
+        states = simple_states if simple else evolved_states
         ict = 0
         for _ in range(bursts):
-            x, y, heading,inf = self.burst(x, y, heading, infected)
-            if inf:
-                ict+=1
+            idx = (x,y)
+            v = infected[idx]
+            direction = dir_states[v]
+            nv = states[v]
+
+            infected[idx] = nv
+
+            if direction is not None:
+                heading = ttable[direction][heading]
+
+            if heading == 'S': y+=1
+            elif heading == 'N': y-=1
+            elif heading == 'E': x+=1
+            else: x-=1
+
+            if nv == 'I':
+                ict += 1
         return(ict)
 
 
